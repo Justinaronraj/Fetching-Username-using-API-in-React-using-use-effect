e00999317d08feba9a9277244d762cc7e8feab7c
Justinaronraj <aron50067@gmail.com>
2024-08-17 11:44:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function Demos() {
+  return (
+    <div className="App">
+      <h1>Hello, World!</h1>
+      <p>Welcome to my React app.</p>
+    </div>
+  );
+}
+
+export default Demos;
